486a3ed0a7b186fbbb2879cfbf012fdf222bf4a6
fede <f.gfaraoni@gmail.com>
2013-11-29 13:09:31 +0100

    Working on form control

--- pilasvacias_com/encuesta2.php
+++ pilasvacias_com/encuesta2.php
@@ -8,49 +8,9 @@ if(!isset($_SESSION['last_email_sent']) || $_SESSION['last_email_sent']==null ||
 	if (isset($_POST["miencuesta"])){
 		$flagError = false;
 
-		$mensaje .= "Sexo: " . $_POST['gender'] . "\n";
-		$mensaje .= "Edad: " . $_POST['age'] . "\n";
+		$emailText = printResultsForEmail();
 
-		$mensaje .= "----------------------------------------------" . "\n";
-
-		$mensaje .= "Pregunta 1: " . $_POST['question1'] . "\n";
-
-		$likes = "";
-		for ($i = 0; $i < 16; $i++) {
-			$q = "question2-" . $i;
-			if (isset($_POST[$q])) {
-				$likes .= $_POST[$q] . " // ";
-			}
-		}
-
-
-		$mensaje .= "Pregunta 2: " . $likes . $_POST['question2-otros'] . "\n";
-
-		$mensaje .= "Pregunta 3: " . $_POST['question3'] . " // " . $_POST['question3-b'] . "\n";
-
-		$mensaje .= "Pregunta 4: " . $_POST['question4'] . "\n";
-		$mensaje .= "Pregunta 5: " . $_POST['question5'] . "\n";
-		$mensaje .= "Pregunta 6: " . $_POST['question6'] . "\n";
-		$mensaje .= "Pregunta 7: " . $_POST['question7'] . "\n";
-
-		$mensaje .= "Pregunta 8: " . $_POST['question8'] . " // " . $_POST['question8-b'] . "\n";
-		$mensaje .= "Pregunta 9: " . $_POST['question9'] . " // " . $_POST['question9-b'] . "\n";
-		$mensaje .= "Pregunta 10: " . $_POST['question10'] . " // " . $_POST['question10-b'] .  "\n";
-
-		$mensaje .= "Pregunta 11: " . $_POST['question11'] . "\n";
-		$mensaje .= "Pregunta 12: " . $_POST['question12'] . "\n";
-
-		$mensaje .= "Fecha: " . date("j-n-Y H:i:s") . "\n";
-
-		
-
-
-		$link = mysqli_connect("mysql51-105.perso","pilasvacias","G7cpcUUkCkdk","pilasvacias") or die("Error " . mysqli_error($link));
-
-		mysqli_query($link,"INSERT INTO encuesta_musica (gender, age, question1, question4, question5, question6)
-			VALUES ('0', '6', '$_POST[question1]', '0', '$_POST[question5]','$_POST[question6]')");
-
-		mysqli_close($link);
+		addResults();
 
 		if($flagError){
 			printError();
@@ -69,6 +29,84 @@ if(!isset($_SESSION['last_email_sent']) || $_SESSION['last_email_sent']==null ||
 }
 
 include("_tail.php");
+
+function printResultsForEmail() {
+		$mensaje .= "Sexo: " . $_POST['gender'] . "\n";
+		$mensaje .= "Edad: " . $_POST['age'] . "\n";
+
+		$mensaje .= "----------------------------------------------" . "\n";
+
+		$mensaje .= "Pregunta 1: " . $_POST['question1'] . "\n";
+
+		$likes = "";
+		for ($i = 0; $i < 16; $i++) {
+			$q = "question2-" . $i;
+			if (isset($_POST[$q])) {
+				$likes .= $_POST[$q] . " // ";
+			}
+		}
+
+
+		$mensaje .= "Pregunta 2: " . $likes . $_POST['question2_otros'] . "\n";
+
+		$mensaje .= "Pregunta 3: " . $_POST['question3'] . " // " . $_POST['question3_b'] . "\n";
+
+		$mensaje .= "Pregunta 4: " . $_POST['question4'] . "\n";
+		$mensaje .= "Pregunta 5: " . $_POST['question5'] . "\n";
+		$mensaje .= "Pregunta 6: " . $_POST['question6'] . "\n";
+		$mensaje .= "Pregunta 7: " . $_POST['question7'] . "\n";
+
+		$mensaje .= "Pregunta 8: " . $_POST['question8'] . " // " . $_POST['question8_b'] . "\n";
+		$mensaje .= "Pregunta 9: " . $_POST['question9'] . " // " . $_POST['question9_b'] . "\n";
+		$mensaje .= "Pregunta 10: " . $_POST['question10'] . " // " . $_POST['question10_b'] .  "\n";
+
+		$mensaje .= "Pregunta 11: " . $_POST['question11'] . "\n";
+		$mensaje .= "Pregunta 12: " . $_POST['question12'] . "\n";
+
+		$mensaje .= "Fecha: " . date("j-n-Y H:i:s") . "\n";
+
+		return $mensaje;
+}
+
+function addResults() {
+	$question1 = $_POST[question1];
+
+
+
+	$question4 = $_POST[question3_];
+
+	$question4 = $_POST[question4];
+
+	$question5 = $_POST[question5];
+
+	$question6 = $_POST[question6];
+
+	$question7 = $_POST[question7];
+
+	$question8 = $_POST[question8];
+
+	$question8_b = $_POST[question8_b];
+
+	$question9 = $_POST[question9];
+
+	$question9_b = $_POST[question9_b];
+
+	$question10 = $_POST[question10];
+
+	$question10_b = $_POST[question10_b];
+
+	$question11 = $_POST[question11];
+
+	$question12 = $_POST[question12];
+
+
+		$link = mysqli_connect("mysql51-105.perso","pilasvacias","G7cpcUUkCkdk","pilasvacias") or die("Error " . mysqli_error($link));
+
+		mysqli_query($link,"INSERT INTO encuesta_musica (gender, age, question1, question4, question5, question6, question7, question8, question8_b, question9, question9_b)
+			VALUES ('1', '3', '$question1', '0', '$question5','$question6', '$question7', '$question8', '$question8_b', '$question9', '$question9_b')");
+
+		mysqli_close($link);
+}
 
 function set_session($attr, $val){
 
@@ -175,72 +213,72 @@ function printError() {
 									<div class = "question">2. ¿Qué tipo de música sueles escuchar?</div><br/>
 									<div class = "check-holder">
 										<div>
-											<input type="checkbox" id ="q2-1" name="question2-0" value="Clásica"> 
+											<input type="checkbox" id ="q2-1" name="question2_0" value="Clásica"> 
 											<label for="q2-1">Clásica</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-2" name="question2-1" value="Dubstep" > 
+											<input type="checkbox" id ="q2-2" name="question2_1" value="Dubstep" > 
 											<label for="q2-2">Dubstep</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-3" name="question2-2" value="Electrónica">
+											<input type="checkbox" id ="q2-3" name="question2_2" value="Electrónica">
 											<label for="q2-3">Electrónica</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-4" name="question2-3" value="Flamenco" > 
+											<input type="checkbox" id ="q2-4" name="question2_3" value="Flamenco" > 
 											<label for="q2-4">Flamenco</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-5" name="question2-4" value="Hip-hop">
+											<input type="checkbox" id ="q2-5" name="question2_4" value="Hip-hop">
 											<label for="q2-5">Hip-hop</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-6" name="question2-5" value="Indie" >
+											<input type="checkbox" id ="q2-6" name="question2_5" value="Indie" >
 											<label for="q2-6">Indie</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-7" name="question2-6" value="Latino">
+											<input type="checkbox" id ="q2-7" name="question2_6" value="Latino">
 											<label for="q2-7">Latino</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-8" name="question2-7" value="Metal" >
+											<input type="checkbox" id ="q2-8" name="question2_7" value="Metal" >
 											<label for="q2-8">Metal</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-9" name="question2-8" value="Musical"> 
+											<input type="checkbox" id ="q2-9" name="question2_8" value="Musical"> 
 											<label for="q2-9">Musical</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-10" name="question2-9" value="Pop" > 
+											<input type="checkbox" id ="q2-10" name="question2_9" value="Pop" > 
 											<label for="q2-10">Pop</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-11" name="question2-10" value="Punk"> 
+											<input type="checkbox" id ="q2-11" name="question2_10" value="Punk"> 
 											<label for="q2-11">Punk</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-12" name="question2-11" value="Rap" > 
+											<input type="checkbox" id ="q2-12" name="question2_11" value="Rap" > 
 											<label for="q2-12">Rap</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-13" name="question2-12" value="Reggae"> 
+											<input type="checkbox" id ="q2-13" name="question2_12" value="Reggae"> 
 											<label for="q2-13">Reggae</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-14" name="question2-13" value="Reggaetón" > 
+											<input type="checkbox" id ="q2-14" name="question2_13" value="Reggaetón" > 
 											<label for="q2-14">Reggaetón</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-15" name="question2-14" value="Rock"> 
+											<input type="checkbox" id ="q2-15" name="question2_14" value="Rock"> 
 											<label for="q2-15">Rock</label>
 										</div>
 										<div>
-											<input type="checkbox" id ="q2-16" name="question2-15" value="Techno" > 
+											<input type="checkbox" id ="q2-16" name="question2_15" value="Techno" > 
 											<label for="q2-16">Techno</label>
 										</div>
 										<br/>
 									</div>
-									Otros: <input type="text" name="question2-otros" value=""><br/>
+									Otros: <input type="text" name="question2_otros" value=""><br/>
 
 									<div class = "question">3. ¿Identificas tu forma de vestir con la música que escuchas?¿Por qué?</div>
 									<div class = "check-holder">
@@ -253,7 +291,7 @@ function printError() {
 											<label for="q3-n">No</label>	
 										</div>
 									</div>
-									<input type="text" name="question3-b" value=""><br/>
+									<input type="text" name="question3_b" value=""><br/>
 
 
 									<div class = "question">4. ¿Escuchas la misma música cuando sales por la noche?</div>
@@ -288,12 +326,12 @@ function printError() {
 									<div class = "question">8. Último disco: </div>
 									<input type="text" name="question8" value=""><br/>
 									<div class = "question">¿Cuándo?</div>
-									<input type="text" name="question8-b" value=""><br/>
+									<input type="text" name="question8_b" value=""><br/>
 
 									<div class = "question">9. Último concierto: </div>
 									<input type="text" name="question9" value=""><br/>
 									<div class = "question">¿Cuándo?</div>
-									<input type="text" name="question9-b" value=""><br/>
+									<input type="text" name="question9_b" value=""><br/>
 
 									<div class = "question">10. ¿Has ido a algún musical? </div>
 									<div class = "check-holder">
@@ -308,7 +346,7 @@ function printError() {
 									</div>
 
 									<div class = "question">¿Cuándo?</div>
-									<input type="text" name="question10-b" value=""><br/>
+									<input type="text" name="question10_b" value=""><br/>
 
 									<div class = "question">11. ¿Tus amistades tienen tus mismos gustos musicales? </div>
 									<div class = "check-holder">
@@ -354,9 +392,9 @@ function printError() {
 							for (var i=0, len=sz.length; i<len; i++) {
 								sz[i].onclick = function() {
 									if (this.value == "Sí") {
-										document.forms["cuestionario"]["question10-b"].disabled = false;
+										document.forms["cuestionario"]["question10_b"].disabled = false;
 									} else if (this.value == "No") {
-										document.forms["cuestionario"]["question10-b"].disabled = true;
+										document.forms["cuestionario"]["question10_b"].disabled = true;
 									}
 								};
 							}
